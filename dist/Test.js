f41b32957c3e85f63094379dc75d6feb5bffd1c4
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2024-07-10 19:57:34 +0700

    feat: Update .gitignore and remove dist/ directory from tracking

--- dist/Test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const MB_1 = __importDefault(require("./MB"));
-(async () => {
-    console.log(MB_1.default);
-})();
